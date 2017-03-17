08437204acf5697af16165383c2ba30e49fcbbd5
Erik Spooner <erik@Air.local>
2017-03-17 14:53:00 -0600

    added a block class

---
+++ Block.h
@@ -0,0 +1,24 @@
+
+#ifndef Block_h
+#define Block_h
+
+#include "chai3d.h"
+
+using namespace chai3d;
+
+class Block
+{
+private:
+  chai3d::cVector3d a;
+  
+  
+public:
+  Block();
+  ~Block();
+  
+  
+};
+
+
+
+#endif /* Block_h */
